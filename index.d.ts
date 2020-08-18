5bb88d7438e276d7b6cfbef4c77bc7fcdcf42dd6
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2020-08-18 12:07:31 +0430

    chore: add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,33 @@
+/**
+ * maintain the defense of borders of modules
+ *
+ * @remarks
+ * provide {@link Margrave} function that returns `margrave` string
+ *
+ * @packageDocumentation
+ */
+
+/**
+ *
+ * return `margrave` string
+ *
+ * @returns the `margrave`  string
+ *
+ * @example
+ * Here's an example:
+ *
+ * ```ts
+ * import { Margrave } from 'margrave'
+ *
+ * console.log(Margrave());
+ * // Prints "margrave":
+ * ```
+ */
+export declare function Margrave(options?: Options): string;
+
+/**
+ * @public
+ */
+export declare interface Options {}
+
+export {};
